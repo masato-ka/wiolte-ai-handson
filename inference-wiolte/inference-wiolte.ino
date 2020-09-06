8c09029dbc54d661c075c0555d535a2411f6b02a
masato-ka <jp6uzv@gmail.com>
2020-09-06 10:05:14 +0900

    Change DNN Network and add confusion matrix.

--- inference-wiolte/inference-wiolte.ino
+++ inference-wiolte/inference-wiolte.ino
@@ -20,31 +20,59 @@
 
 WioLTE Wio;
 HardwareTimer Timer3(TIM3);
-ADXL345 Accel;
+
+//Status
 bool is_buzzer_on = false;
+bool is_send = false;
+short counter = 0;
+char old_data[1]={0x00};
+
+ADXL345 Accel;
 
 tfmicro_wrapper::TfMicroWrapper tfMicroWrapper;
 TfLiteTensor* input = nullptr;
 TfLiteTensor* output = nullptr;
-float min_val=23.388031127053;
-float max_val=686.9454126784748;
+float mean = 277.92817024;
+float sigma = 73.88031294;
 
-//Callback for Timer3(Buzzer controll)
-void buzzer(HardwareTimer *HT){
-  digitalWrite(BUZZER_PIN, is_buzzer_on);
+//Hardware interrupt.
+void buzzer(){
   is_buzzer_on = !is_buzzer_on;
+}
+
+//Callback for Timer3
+void send_event(HardwareTimer* HT){
+  counter++;
+  if(counter > 10){
+    is_send = true;
+    counter = 0;
+  }
 }
 
 //Send to unified endpoint.
 int send_unified(char* data){
+  char rcv_data[1024];
   int connectId = Wio.SocketOpen("uni.soracom.io", 23080, WIOLTE_UDP);
   if ( connectId < 0){
+    
     return connectId;
   }
-
   if(!Wio.SocketSend(connectId, data)){
+    goto err_close;
+  }
+  int length;
+  length = Wio.SocketReceive(connectId, rcv_data, sizeof (rcv_data), 10000);
+  if (length < 0) {
+    goto err_close;
+  }
+  if (length == 0) {
+    goto err_close;
+  }
+err_close:
+  if (!Wio.SocketClose(connectId)) {
     return -1;
   }
+  
   return 1;
 }
 
@@ -68,19 +96,23 @@ void setup() {
   input = tfMicroWrapper.getInput();
   output = tfMicroWrapper.getOutput();
 
-  // Start BUZZER
+  // Start Intermittent transmission
   Timer3.setOverflow(1000 * 1000, MICROSEC_FORMAT); // = per 1 sec
-  Timer3.attachInterrupt(&buzzer);
+  Timer3.attachInterrupt(&send_event);
   Timer3.resume();
+  
+  
   pinMode(BUZZER_PIN, OUTPUT);
   
-  delay(500);
-  
+  //Switch Interrupt
+  pinMode(BUTTON_PIN, INPUT);
+  attachInterrupt(BUTTON_PIN, buzzer, RISING);
+
+  delay(500);  
 }
 
 void loop() {
-
-  char data[1024];
+  char data[1];
   //加速度センサー処理
   // Place our calculated x value in the model's input tensor
   int x;
@@ -90,7 +122,7 @@ void loop() {
     Accel.readXYZ(&x, &y, &z);
     float l = pow(x,2)+pow(y,2)+pow(z,2);
     float l2 = sqrt(l);
-    float norm_l2 = (l2-min_val)/(max_val-min_val);
+    float norm_l2 = (l2-mean)/sigma;
     input->data.f[i] = norm_l2;
     delay(INTERVAL);
   }
@@ -102,10 +134,20 @@ void loop() {
   if(output->data.f[0] < output->data.f[1]){
     SerialUSB.println("Buzzer!");
     Wio.LedSetRGB(10,10,10);
-    sprintf(data, "{\"\":%s}", "BUZZER");
-    send_unified(data);
+    data[0]=0x01; //Buzzer ON
   }else{
     Wio.LedSetRGB(0,0,0);
+    data[0]=0x02; //Buzzer OFF
   }
-   
+
+  //When change state and intermittent transmission
+  if(old_data[0] != data[0] || is_send){    
+    int i = send_unified(data);
+    is_send =false;
+  }
+  old_data[0] = data[0];
+
+  //Buzzer ON/OFF
+  digitalWrite(BUZZER_PIN, is_buzzer_on);
+  
 }
